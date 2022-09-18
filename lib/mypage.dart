d998a10f82184d53a6b350f5b0e3e000149d44ae
mono <you@example.com>
2022-09-18 09:13:19 +0900

    プロフィール、試合結果画面に画像表示

--- lib/mypage.dart
+++ lib/mypage.dart
@@ -1,8 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:cloud_firestore_web/cloud_firestore_web.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'dart:io';
 import 'main.dart';
 import 'login.dart';
 import 'editprofile.dart';
@@ -11,7 +13,6 @@ class MyPage extends StatelessWidget {
   MyPage();
   String name = 'name';
   int rating = 0;
-
   @override
   Widget build(BuildContext context) {
     // ユーザー情報を受け取る
@@ -25,10 +26,12 @@ class MyPage extends StatelessWidget {
         builder: (context, snapshot) {
           if (snapshot.hasData) {
             final List<DocumentSnapshot> documents = snapshot.data!.docs;
-            return _MyPage(
-                name: documents[0]['name'],
-                id: user.uid,
-                rating: documents[0]['rating']);
+            return MyPageState(
+              name: documents[0]['name'],
+              id: user.uid,
+              rating: documents[0]['rating'],
+              imgUrl: documents[0]['imgURL'],
+            );
           }
           return Center(
             child: SizedBox(height: 8),
@@ -37,35 +40,48 @@ class MyPage extends StatelessWidget {
   }
 }
 
-class _MyPage extends StatelessWidget {
-  final String name;
-  final String id;
-  final int rating;
-  const _MyPage({
-    Key? key,
-    required this.name,
-    required this.id,
-    required this.rating,
-  }) : super(key: key);
+class MyPageState extends StatefulWidget {
+  String name;
+  String id;
+  String imgUrl;
+  int rating;
+  MyPageState(
+      {Key? key,
+      required this.name,
+      required this.id,
+      required this.rating,
+      required this.imgUrl})
+      : super(key: key);
+
+  @override
+  _MyPageState createState() =>
+      _MyPageState(this.name, this.id, this.rating, this.imgUrl);
+}
+
+class _MyPageState extends State<MyPageState> {
+  _MyPageState(this.name, this.id, this.rating, this.imgUrl);
+  String name;
+  String id;
+  String imgUrl;
+  int rating;
+  Image? _img;
+
+  Future<void> IndicateImage() async {
+    FirebaseStorage storage = FirebaseStorage.instance;
+    Reference imageRef = storage.ref(imgUrl);
+    String imageUrl = await imageRef.getDownloadURL();
+    setState(() {
+      _img = Image.network(imageUrl);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
+    IndicateImage();
     return Scaffold(
       body: Center(
           child: Column(mainAxisSize: MainAxisSize.min, children: [
-        ClipOval(
-          child: Container(
-            color: Colors.greenAccent,
-            width: 48,
-            height: 48,
-            child: Center(
-              child: Text(
-                name.substring(0, 1),
-                style: TextStyle(color: Colors.white, fontSize: 24),
-              ),
-            ),
-          ),
-        ),
+        if (_img != null) _img!,
         SizedBox(height: 8),
         Text(name),
         SizedBox(height: 8),
@@ -73,40 +89,41 @@ class _MyPage extends StatelessWidget {
         SizedBox(height: 8),
         Text('レート：${rating}'),
         SizedBox(height: 8),
-        Container(
-          child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-            SizedBox(
-              width: 125,
-              child: ElevatedButton(
-                child: Text('編集'),
-                onPressed: () async {
-                  await Navigator.of(context).pushReplacement(
-                    MaterialPageRoute(builder: (context) {
-                      return EditProfilePage(name: name);
-                    }),
-                  );
-                },
-              ),
-            ),
-            SizedBox(width: 8),
-            SizedBox(
+        if (_img != null)
+          Container(
+            child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+              SizedBox(
                 width: 125,
                 child: ElevatedButton(
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all(Colors.red),
-                  ),
-                  child: Text('ログアウト'),
+                  child: Text('編集'),
                   onPressed: () async {
-                    await FirebaseAuth.instance.signOut();
                     await Navigator.of(context).pushReplacement(
                       MaterialPageRoute(builder: (context) {
-                        return LoginPage();
+                        return EditProfilePage(name: name, img: _img!);
                       }),
                     );
                   },
-                )),
-          ]),
-        ),
+                ),
+              ),
+              SizedBox(width: 8),
+              SizedBox(
+                  width: 125,
+                  child: ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.red),
+                    ),
+                    child: Text('ログアウト'),
+                    onPressed: () async {
+                      await FirebaseAuth.instance.signOut();
+                      await Navigator.of(context).pushReplacement(
+                        MaterialPageRoute(builder: (context) {
+                          return LoginPage();
+                        }),
+                      );
+                    },
+                  )),
+            ]),
+          ),
       ])),
     );
   }
